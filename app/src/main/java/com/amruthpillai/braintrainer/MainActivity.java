bde3d0fc3bbce8f592443d6910cdfdd3d5cbc703
Amruth Pillai <im.amruth@gmail.com>
2017-07-23 17:24:30 +0530

    created the initial UI

--- app/src/main/java/com/amruthpillai/braintrainer/MainActivity.java
+++ app/src/main/java/com/amruthpillai/braintrainer/MainActivity.java
@@ -2,12 +2,22 @@ package com.amruthpillai.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGameButton = (Button) findViewById(R.id.startButton);
+    }
+
+    public void startGame(View view) {
+        startGameButton.setVisibility(View.INVISIBLE);
     }
 }
